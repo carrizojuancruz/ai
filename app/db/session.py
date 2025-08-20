a66903c10ab6bb434e21117912c685497827feac
Gonzalo Pereira <49208619+gonzape27@users.noreply.github.com>
2025-08-20 10:16:56 -0300

    Update session.py

--- app/db/session.py
+++ app/db/session.py
@@ -5,9 +5,9 @@ from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://verde:verde@localhost:5432/verde")
+DATABASE_URL = os.getenv("DATABASE_ASYNC_URL", "postgresql+asyncpg://verde:verde@localhost:5432/verde")
 
-engine = create_async_engine(DATABASE_URL, echo=False, future=True)
+engine = create_async_engine(DATABASE_ASYNC_URL, echo=False, future=True)
 
 async_session_factory = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
